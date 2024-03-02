a35fcd1ab90cde8ffc3655f991c55e261bfd9150
Roberto09 <roberto.gt1509@gmail.com>
2024-03-01 18:28:55 -0800

    fix holistic prunning

--- prunners.py
+++ prunners.py
@@ -54,18 +54,18 @@ def prune_mlps_holistically(importances, prune_ratio):
 
     # Choose which node-indexes to prune, mark those indexes with '0'
     _, indices_to_replace = torch.topk(concat_imps, num_prune_cells, largest=False)
-    concat_imps[indices_to_replace] = 0
+    mask = torch.ones_like(concat_imps, dtype=torch.bool)
+    mask[indices_to_replace] = False
     
     # Make a new dict with indexes with smallest values zeroed out
     split_size = len(list(importances.values())[0])
 
-    pruned_tensors = torch.split(concat_imps, split_size)
+    pruned_tensors = torch.split(mask, split_size)
     pruned_tensor_dict = {key: tensor for key, tensor in zip(importances.keys(), pruned_tensors)}
 
     # Prune each mlp
     for mlp, keep_idx in pruned_tensor_dict.items():
-        keep_idx = keep_idx[keep_idx != 0]
-
+        keep_idx = torch.arange(keep_idx.shape[0], dtype=torch.long)[keep_idx]
         fc1 = mlp.fc1
         dtype = fc1.weight.dtype
         fc1_pruned = torch.nn.Linear(
@@ -74,8 +74,8 @@ def prune_mlps_holistically(importances, prune_ratio):
             dtype=dtype
         )
         with torch.no_grad():
-            fc1_pruned.weight.data = torch.clone(fc1.weight[keep_idx.long()])
-            fc1_pruned.bias.data = torch.clone(fc1.bias[keep_idx.long()])
+            fc1_pruned.weight.data = torch.clone(fc1.weight[keep_idx])
+            fc1_pruned.bias.data = torch.clone(fc1.bias[keep_idx])
 
         fc2 = mlp.fc2
         dtype = fc2.weight.dtype
@@ -85,7 +85,7 @@ def prune_mlps_holistically(importances, prune_ratio):
             dtype=dtype
         )
         with torch.no_grad():
-            fc2_pruned.weight.data = torch.clone(fc2.weight[:, keep_idx.long()])
+            fc2_pruned.weight.data = torch.clone(fc2.weight[:, keep_idx])
 
         mlp.fc1 = fc1_pruned
         mlp.fc2 = fc2_pruned
