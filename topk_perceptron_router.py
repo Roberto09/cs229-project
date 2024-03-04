bf5b4ea25cd720d3402fdcf514938f3c3e294bd7
AndriFV <afv6@hi.is>
2024-03-03 18:12:36 -0800

    change from switch to topk routing

--- topk_perceptron_router.py
+++ topk_perceptron_router.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 import pickle
 import numpy as np
 
-
 # this is assuming 8 experts
 def init_weights_by_centroids(layer):
     with open('cluster_pkl/clustering_embeddings.pkl', 'rb') as file:
@@ -14,25 +13,23 @@ def init_weights_by_centroids(layer):
     
     return torch.tensor(centroids, dtype = torch.float)
 
-# https://arxiv.org/pdf/2101.03961.pdf
-class SwitchPerceptronRouter(nn.Module):
-    def __init__(self, input_size, n_experts, layer):
+# For switch routing(https://arxiv.org/pdf/2101.03961.pdf) use k = 1
+class TopKPerceptronRouter(nn.Module):
+    def __init__(self, input_size, n_experts, layer, k):
         super().__init__()
+        self.k = k
         self.fc = nn.Linear(input_size, n_experts)
         self.fc.weight = nn.Parameter(init_weights_by_centroids(layer))
         
     def forward(self, x):
         logits = self.fc(x)
         softmax_values = F.softmax(logits, dim=1)
-        expert_idx = torch.argmax(softmax_values, dim=1, keepdim=True) # index of expert to route to
-        router_gate_values = torch.gather(softmax_values, 1, expert_idx)
-        return expert_idx, router_gate_values # expert, reouter gate value
+        top_k_expert_weights, top_k_experts_idx = torch.topk(softmax_values, self.k, dim=1)
+        return top_k_experts_idx, top_k_expert_weights # expert indices, expert_weights
 
 if __name__ == '__main__':
-     layer = 12
-     router = SwitchPerceptronRouter(2048, 8, layer)
-     
      # test functionality in context
+     layer = 12
      with open('cluster_pkl/clustering_embeddings.pkl', 'rb') as file:
             clustered_embeddings = pickle.load(file)
 
@@ -40,7 +37,11 @@ if __name__ == '__main__':
      idx = np.random.choice(embeddings.shape[0], size=50, replace=False)
      embeddings = embeddings[idx]
      embeddings = torch.tensor(embeddings, dtype = torch.float)
+     
+     router = TopKPerceptronRouter(2048, 8, layer, k=2)
+     experts_routed, expert_weights = router.forward(embeddings)
 
-     experts_routed, gate_values = router.forward(embeddings)
-
+     router = TopKPerceptronRouter(2048, 8, layer, k=1)
+     experts_routed_, expert_weights_ = router.forward(embeddings)
+     
      print()
