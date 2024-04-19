f0b2b758d90d15ece4b702b9a7907ca9b5679ea0
Roberto09 <roberto.gt1509@gmail.com>
2024-04-19 16:30:25 -0700

    move dataset preprocessing to dataset.py

--- dataset.py
+++ dataset.py
@@ -2,6 +2,89 @@ import os
 from datasets import load_dataset, Dataset
 import numpy as np
 import pandas as pd
+from transformers import AutoTokenizer
+from collections import defaultdict, Counter
+import pickle
+import random
+import pickle
+from itertools import chain
+
+def fetch_preprocessed_data(data="nampdn-ai/tiny-textbooks", filedir="./"):
+    """ Gets data from huggingface and:
+    * Tokenizes the whole dataset and pickles it to filedir/dataset_tokenized.pkl
+    * Creates a map of token to sample and pickles it to filedir/token_row_map.pkl
+    
+    If filedir/{dataset_tokenized.pkl, filedir/token_row_map.pkl} already exist,
+    it just reads the values and returns them
+    """
+
+    tokenized_dataset_path = f"{filedir}/dataset_tokenized.pkl"
+    token_row_map_path = f"{filedir}/token_row_map.pkl"
+    if os.path.isfile(tokenized_dataset_path) and os.path.isfile(token_row_map_path):
+        return pd.read_pickle(tokenized_dataset_path), pd.read_pickle(token_row_map_path)
+
+    dataset = load_dataset(data)
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
+
+    num_rows = dataset['train'].num_rows
+    dataset_tokenized = [tokenizer.encode(dataset['train'][i]['text']) for i in range(num_rows)]
+
+    token_row_map = defaultdict(set)
+
+    for i, row in enumerate(dataset_tokenized):
+        for token in row:
+            token_row_map[token].add(i)
+    
+    pd.to_pickle(dataset_tokenized, tokenized_dataset_path)
+    pd.to_pickle(token_row_map, token_row_map_path)
+    return dataset_tokenized, token_row_map_path
+
+
+class TokenInfo():
+
+    # TODO: maybe take dataset name as argument, difficult because they dont all have the same structure
+    def __init__(self, filedir="./"):        
+        print('...Loading dataset...')
+        with open(f"{filedir}/dataset_tokenized.pkl", "rb") as f:
+            self.dataset_tokenized = pickle.load(f)
+        
+        with open(f"{filedir}/token_row_map.pkl", "rb") as f:
+            self.token_row_map = pickle.load(f)
+
+        self.token_counts = Counter(list(chain(*self.dataset_tokenized)))
+        self.tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
+  
+        print('...Loading complete...')
+    
+    def get_prefixes_plus_one(self, token, prefix_len, n):
+        # gets n samples consisting of [prefix_len-1 tokens] +  [token] + [1 suffix token]
+        token_rows = list(self.token_row_map[token])
+        out = []
+        while len(out) < n:
+            row = random.choice(token_rows)
+            row_tokens = self.dataset_tokenized[row]
+            # get index if it:
+            # 1. matches the token
+            # 2. there's at least prefix_len tokens before it
+            # 3. it's not the last token
+            token_idx = [index for index, value in enumerate(row_tokens)
+                         if value == token and index >= prefix_len and index != len(row_tokens)-1]
+            if len(token_idx) > 0:
+                i = random.sample(token_idx, 1)[0]
+                # gets a sample consisting of [prefix_len-1 tokens] +  [token] + [1 suffix token]
+                sample = row_tokens[i-prefix_len+1: i+2]
+                assert len(sample) == prefix_len
+                out.append(sample)
+
+        return out
+    
+    def get_prefixes(self, token, prefix_len, n):
+        assert False, "get_prefixes is deprecated, use get_prefixes_plus_one instead"
+
+    def top_n(self, n):
+        top_tokens =  self.token_counts.most_common(n)
+        return [(x[0], self.tokenizer.decode(x[0]), x[1]) for x in top_tokens]
+
 
 def get_baseline_dataset(filename="./baseline_dataset.pkl"):
     """Returns tiny-textbooks dataset with 52 examples and caches it
@@ -23,3 +106,13 @@ def get_baseline_dataset(filename="./baseline_dataset.pkl"):
     dataset = dataset.train_test_split(test_size=2000, shuffle=True, seed=123)
     pd.to_pickle(dataset, "./baseline_dataset.pkl")
     return dataset
+
+
+if __name__ == '__main__':
+    # EXAMPLE USAGE
+    token_info = TokenInfo()
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
+    prefix_test = token_info.get_prefixes(tokenizer.encode('dog')[0], 10, 5)
+    top_tokens = token_info.top_n(100)
+
+    print(prefix_test)
