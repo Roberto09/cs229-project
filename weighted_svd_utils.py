407468b8787be397f8b1c7324fa2f3a2794f6899
Roberto09 <roberto.gt1509@gmail.com>
2024-04-21 23:59:38 -0700

    Fix weighted_svd_inverse_weight and WeightedSVD

--- weighted_svd_utils.py
+++ weighted_svd_utils.py
@@ -66,76 +66,6 @@ def toy_two_subspace_matrix(inp=80, out=100, r=32, r2=None, noise=False):
     same_subspace_indices = [i for i in range(out//2)]
     return m, same_subspace_indices
 
-def weighted_svd_inverse_weight(M, W):
-    """
-    Uses inverse-weight technique to come up with weighted SVD.
-    M and W should be the same shape, M is the matrix, W are the weights
-    """
-    should_transpose = False
-    if M.shape[0] > M.shape[1]:
-        should_transpose = True
-        M = M.T
-        W = W.T
-    W = torch.sqrt(W)
-    M_right_inv = torch.linalg.solve(M.T @ M, M.T)
-    # solves: W'M = W(*)M => W' = (W(*)M)M^-1
-    W_prime = (W*M) @ M_right_inv
-    U, S, V = get_svd(W*M)
-    U = torch.linalg.solve(W_prime, U)
-    if should_transpose:
-        U, V = V, U
-    return U, S, V
-
-class WeightedSVD(nn.Module, object):
-    """
-    Computes weighted SVD given arbitrary weights using GD.
-    Send to CUDA for maximum efficiency:
-    w_svd = WeightedSVD(...).cuda()
-    """
-    def __init__(self, M, W, rank):
-        super().__init__()
-        out, inp = M.shape
-        self.M = M.detach()
-        self.W = W.detach()
-        self.U = nn.Parameter(torch.randn(out, rank))
-        self.V = nn.Parameter(torch.randn(inp, rank))
-        self.S = nn.Parameter(torch.randn(rank))
-        self.rank=rank
-    
-    def forward(self, x):
-        return self.AB()@x
-
-    def fit(self, iters=20000, lr=0.01, lambd=1):
-        """ Fits WeightedSVD and returns U, S, V
-        """
-        errs = []
-        optimizer = optim.Adam(self.parameters(), lr=lr)
-        lr_scheduler = StepLR(optimizer, iters//10, gamma=0.8) # basically lr becomes 1/10 of start lr by end of training
-        for i in tqdm(range(iters)):
-            optimizer.zero_grad()
-            loss = weighted_frobenious(self.M, (self.U*self.S)@self.V.T, w=self.W)
-            
-            errs.append(loss.detach())
-            
-            ortho_penalty_u = weighted_frobenious(self.U.T@self.U, torch.eye(self.U.shape[1]))
-            ortho_penalty_v = weighted_frobenious(self.V.T@self.V, torch.eye(self.V.shape[1]))
-            
-            loss = loss + lambd * (ortho_penalty_u + ortho_penalty_v)
-            loss.backward()
-            optimizer.step()
-            lr_scheduler.step()
-        self.fix_USV()
-        # return errs
-        return self.U.detach().clone(), self.S.detach().clone(), self.V.detach().clone()
-    
-    def fix_USV(self):
-        # Makes sure USV matches desired format
-        self.U.data = self.U*torch.sign(self.S)
-        self.S.data = torch.abs(self.S)
-        perm = torch.argsort(self.S, descending=True)
-        self.S.data = self.S[perm]
-        self.U.data = self.U[:, perm]
-        self.V.data = self.V[:, perm]
 
 def toy_seven_subspace_matrix(inp=80, out=100, r=6, noise=False):
     """
@@ -175,3 +105,75 @@ def toy_seven_subspace_matrix_unqual_size(inp=250, out=896, r=4, noise=True):
         m += torch.randn(m.shape)*0.1
     #same_subspace_indices = [i for i in range(out//8)]
     return m#, same_subspace_indices
+
+
+def weighted_svd_inverse_weight(M, W):
+    """
+    Uses inverse-weight technique to come up with weighted SVD.
+    M and W should be the same shape, M is the matrix, W are the weights
+    """
+    should_transpose = False
+    if M.shape[0] > M.shape[1]:
+        should_transpose = True
+        M = M.T
+        W = W.T
+    W = torch.sqrt(W)
+    M_left_inv = torch.linalg.pinv(M)
+    # solves: MW' = W(*)M => W' = M^-1(W(*)M)
+    W_prime = M_left_inv @ (W*M)
+    U, S, V = get_svd(W*M)
+    # U = torch.linalg.solve(W_prime, U)
+    V = (V.T @ torch.linalg.pinv(W_prime)).T 
+    if should_transpose:
+        U, V = V, U
+    return U, S, V
+
+class WeightedSVD(nn.Module, object):
+    """
+    Computes weighted SVD given arbitrary weights using GD.
+    Send to CUDA for maximum efficiency:
+    w_svd = WeightedSVD(...).cuda()
+    """
+    def __init__(self, M, W, rank):
+        super().__init__()
+        out, inp = M.shape
+        self.M = M.detach()
+        self.W = W.detach()
+        self.U = nn.Parameter(torch.randn(out, rank))
+        self.V = nn.Parameter(torch.randn(inp, rank))
+        self.S = nn.Parameter(torch.randn(rank))
+        self.rank=rank
+    
+    def forward(self, x):
+        return self.AB()@x
+
+    def fit(self, iters=2400, lr=0.01, lambd=1):
+        """ Fits WeightedSVD and returns U, S, V
+        """
+        errs = []
+        optimizer = optim.Adam(self.parameters(), lr=lr)
+        lr_scheduler = StepLR(optimizer, iters//12, gamma=0.6) # basically lr becomes 1/10 of start lr by end of training
+        for i in tqdm(range(iters)):
+            optimizer.zero_grad()
+            loss = weighted_frobenious(self.M, (self.U*self.S)@self.V.T, w=self.W)
+            
+            errs.append(loss.detach().clone())
+            
+            ortho_penalty_u = weighted_frobenious(self.U.T@self.U, torch.eye(self.U.shape[1]))
+            ortho_penalty_v = weighted_frobenious(self.V.T@self.V, torch.eye(self.V.shape[1]))
+            
+            loss = loss + lambd * (ortho_penalty_u + ortho_penalty_v)
+            loss.backward()
+            optimizer.step()
+            lr_scheduler.step()
+        self.fix_USV()
+        return self.U.detach().clone(), self.S.detach().clone(), self.V.detach().clone()
+    
+    def fix_USV(self):
+        # Makes sure USV matches desired format
+        self.U.data = self.U*torch.sign(self.S)
+        self.S.data = torch.abs(self.S)
+        perm = torch.argsort(self.S, descending=True)
+        self.S.data = self.S[perm]
+        self.U.data = self.U[:, perm]
+        self.V.data = self.V[:, perm]
